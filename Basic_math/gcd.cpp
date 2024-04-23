b26ac887d01de50df09c1d21b32dc6914fda3f0a
vishal m k <vishalmk@vishals-MacBook-Air.local>
2024-04-23 19:04:03 +0530

    basic

---
+++ Basic_math/gcd.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    while (a>0 && b>0)
+    {
+        if(a>b) a=a%b;
+        else b=b%a;
+    }
+    if (a==0) cout << b;
+    else cout << a;
+}
