5e0860dd2989a041a7f6d76e404e415576b00ea6
vishal m k <mkvishal30@gmail.com>
2024-11-08 00:02:23 +0530

    array_lec2

---
+++ Arrays/Lecture2/2sum.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+
+
+using namespace std;
+
+vector<int> twoSum(vector<int> a, int target){
+    map<int,int> mpp;
+    for (int i = 0; i < a.size(); i++)
+    {
+        /* code */
+        int num=a[i];
+        int more = target - num;
+        if (mpp.find(more)!= mpp.end())
+        {
+            /* code */
+            return {mpp[more],i};
+        }
+        mpp[num]=i;
+    }
+    return {-1,-1};
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cin >> arr[i];
+    }
+    int target;
+    cin >> target;
+    vector<int> result = twoSum(arr,target);
+    cout << result[0] << " " << result[1] << endl;
+    return 0;
+}
+
+// brute force
+// vector<int> twoSum(vector<int> arr,int target){
+//     int n = arr.size();
+//     for (int i = 0; i < n; i++)
+//     {
+//         /* code */
+//         for (int j = i+1; j < n; j++)
+//         {
+//             /* code */
+//             if (arr[i]+arr[j]==target)
+//             {
+//                 return {i,j};
+//             }
+//         }
+//     }
+// }
