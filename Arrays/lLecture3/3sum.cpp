7ef730524110582d279ae72caa5c7fac4192c2be
vishal m k <mkvishal30@gmail.com>
2024-12-10 20:12:59 +0530

    array_lec3

---
+++ Arrays/lLecture3/3sum.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+        int n= nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum<0){
+                    j++;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    vector<int> temp= {nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        // for (int i = 0; i < ans.size(); i++)
+        // {
+        //     /* code */cout << ans[i] << endl;
+        // }
+        return ans;
+    }
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cin >> arr[i];
+    }
+    threeSum(arr);
+    return 0;
+}
