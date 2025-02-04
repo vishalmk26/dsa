8fc862b8ee24afa0f373a10e0fded1b6a86801a1
vishal m k <mkvishal30@gmail.com>
2025-02-04 18:50:41 +0530

    LL1

--- LinkedList/Basics/insert.cpp
+++ LinkedList/Basics/insert.cpp
@@ -33,11 +33,94 @@ Node* convertArr2Ll(vector<int> arr){
     return head;
 }
 
+void printLL(Node* head){
+    while(head){
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+// count the number of elements
+int getCount(Node* head) {
+    // Code here
+    Node* temp = head;
+    int cnt = 0;
+    while(temp != NULL){
+        cnt++;
+        temp = temp->next;
+    }
+    return cnt;
+}
+
+// Insert at the head of a Linked List
+Node* insertHead(Node* head, int val){
+    Node* temp = new Node(val);
+    temp->next = head;
+    return temp;
+}
+
+Node* insertLast(Node* head, int val){
+    if(head == NULL) return new Node(val); 
+    Node* temp = head;
+    Node* last = new Node(val);
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = last;
+    return head;
+}
+
+Node* insertk(Node* head, int val, int k){
+    if(head == NULL){
+        if(k == 1) return new Node(val);
+        else return NULL;
+    }
+    if(k == 1){
+        Node* x = new Node(val, head);
+        return x;
+    }
+    int cnt = 0;
+    Node* temp = head;
+    while(temp!=NULL){
+        cnt++;
+        if(cnt == k-1){
+            Node* newNode = new Node(val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+
+Node* insertBeforeVal(Node* head, int el, int val){
+    if(head == NULL){
+        return NULL;
+    }
+    if(head->data == val){
+        return new Node(el, head);
+    }
+    Node* temp = head;
+    while(temp->next !=NULL){
+        if(temp->next->data == val){
+            Node* newNode = new Node(el);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+
 int main(){
     vector<int> arr = {2, 5, 8, 7};
     Node* head = convertArr2Ll(arr);
-    // head = deletek(head, 7);
-    head = deleteVal(head, 7);
+    head = insertBeforeVal(head, 4, 8);
     printLL(head);
+    // int n = getCount(head);
+    // cout << n;
     return 0;
 }
