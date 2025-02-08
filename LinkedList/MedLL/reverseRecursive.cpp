0910855dfb05e606c54bc05e3d0af6da69de8c08
vishal m k <mkvishal30@gmail.com>
2025-02-08 14:15:08 +0530

    ll

---
+++ LinkedList/MedLL/reverseRecursive.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    public:
+    Node(int data1, Node* next1, Node* prev1){
+        data = data1;
+        next = next1;
+        prev = prev1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+Node* convertArr2Ll(vector<int>& arr) {
+    // Write your code here
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i = 1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = prev->next;
+    }
+    return head;
+}
+
+void printLL(Node* head){
+    while(head){
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+Node* reverseLinkedList(Node *head)
+{
+    // Write your code here
+    if(head == NULL || head->next == NULL) return head;
+    Node* newHead = reverseLinkedList(head->next);
+    Node* front = head->next;
+    front->next = head;
+    head->next = nullptr;
+    return newHead;
+}
+
+int main(){
+    vector<int> arr = {2, 5, 8, 7};
+    Node* head = convertArr2Ll(arr);
+    head = reverseLinkedList(head);
+    printLL(head);
+    // int n = getCount(head);
+    // cout << n;
+    return 0;
+}
