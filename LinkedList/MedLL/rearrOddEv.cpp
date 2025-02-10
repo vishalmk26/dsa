80d951d7beae9ab3c680077ff8933cede7cab7af
vishal m k <mkvishal30@gmail.com>
2025-02-10 17:58:22 +0530

    ll

---
+++ LinkedList/MedLL/rearrOddEv.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    public:
+    Node(int data1, Node* next1, Node* prev1){
+        data = data1;
+        next = next1;
+        prev = prev1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+Node* convertArr2Ll(vector<int>& arr) {
+    // Write your code here
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i = 1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = prev->next;
+    }
+    return head;
+}
+
+void printLL(Node* head){
+    while(head){
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+Node* oddEvenList(Node* head)
+{
+	// Write your code here.
+            if (head == NULL || head->next == NULL) return head;
+        Node* odd = head;
+        Node* even = head->next;
+        Node* evenHead = even;
+        while(even != NULL && even->next!= NULL){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+}
+
+int main(){
+    vector<int> arr = {2, 5, 8, 7};
+    Node* head = convertArr2Ll(arr);
+    head = oddEvenList(head);
+    printLL(head);
+    // int n = getCount(head);
+    // cout << n;
+    // cout <<  detectCycle(head);
+    return 0;
+}
