44997f7abedf0632bcd5bb93655d8991093635ab
vishal m k <mkvishal30@gmail.com>
2025-03-16 12:50:52 +0530

    stack

---
+++ StackQueue/Lec3/NextGreaterEl.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+// CIRCULAR ARRAY:
+vector<int> nextGreaterElements(vector<int>& arr){
+    int n = arr.size();
+    vector<int> ans(n, -1);
+    stack<int> st;
+    for(int i = 2*n-1; i>=0; i--){
+        while(!st.empty() and st.top()<=arr[i%n]){
+            st.pop();
+        }
+        if(i<n) if(!st.empty()) ans[i] = st.top();
+        st.push(arr[i%n]);
+    }
+    return ans;
+}
+
+// NON CIRCULAR ARRAY:
+// vector<int> nextGreaterElements(vector<int>& arr){
+//     int n = arr.size();
+//     vector<int> ans(n, -1);
+//     stack<int> st;
+//     for(int i = n-1; i>=0; i--){
+//         while(!st.empty() and st.top()<=arr[i]){
+//             st.pop();
+//         }
+//         if(st.empty()) ans[i] = -1;
+//         else ans[i] = st.top();
+//         st.push(arr[i]);
+//     }
+//     return ans;
+// }
+
+int main() {
+  vector < int > v = {5,7,1,2,6,0};
+  vector < int > res = nextGreaterElements(v);
+  cout << "The next greater elements are" << endl;
+  for (int i = 0; i < res.size(); i++) {
+    cout << res[i] << " ";
+  }
+  return 0;
+}
